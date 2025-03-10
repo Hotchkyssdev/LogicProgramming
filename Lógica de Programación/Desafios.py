5a3e38d57ca17161576a897d4ff5401b3d111a9d
HotchkyssTomás <128932163+Hotchkyssdev@users.noreply.github.com>
2025-03-10 18:58:56 -0300

    Add files via upload

--- Lógica de Programación/Desafios.py
+++ Lógica de Programación/Desafios.py
@@ -230,3 +230,56 @@ def fibonacci(numero: int) -> int:
 
 posicion = int(input("Ingrese la posición de la sucesion de fibonacci: "))
 print(fibonacci(posicion))
+
+'''
+Semana 7:
+
+DIFICULTAD EXTRA (opcional):
+- Utilizando la implementación de pila y cadenas de texto, simula el mecanismo adelante/atrás
+ *   de un navegador web. Crea un programa en el que puedas navegar a una página o indicarle
+ *   que te quieres desplazar adelante o atrás, mostrando en cada caso el nombre de la web.
+ *   Las palabras "adelante", "atrás" desencadenan esta acción, el resto se interpreta como
+ *   el nombre de una nueva web.
+- Utilizando la implementación de cola y cadenas de texto, simula el mecanismo de una
+ *   impresora compartida que recibe documentos y los imprime cuando así se le indica.
+ *   La palabra "imprimir" imprime un elemento de la cola, el resto de palabras se
+ *   interpretan como nombres de documentos.
+'''
+
+def navegadorweb():
+    pila = []
+    while True:
+        accion = input("Añade una url o interactúa con palabras adelante/atrás/salir: ")
+        if accion == "salir":
+            print("Saliendo del navegador.")
+            break
+        elif accion == "adelante":
+            pass
+        elif accion == "atras":
+            if len(pila) > 0:
+                pila.pop()
+        else:
+            pila.append(accion)
+
+        if len(pila) > 0:
+            print(f"Has navegado a la web: {pila[len(pila) - 1]}")
+        else:
+            print("Estás en la página de inicio.")
+
+navegadorweb()
+
+def impresora_compartida():
+    cola = []
+    while True:
+        accion = input("Añade un documento o selecciona imprimir/salir: ")
+        if accion == "salir":
+            break
+        elif accion == "imprimir":
+            if len(cola) > 0:
+                print(f"Imprimiendo: {cola.pop(0)}")
+        else:
+            cola.append(accion)
+        
+        print(f"Cola de Impresión: {cola}")
+
+impresora_compartida()
