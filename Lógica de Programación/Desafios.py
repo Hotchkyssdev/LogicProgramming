60d01352f6c8b6f051bc9839ebf2521e8e44a1b7
HotchkyssTomás <128932163+Hotchkyssdev@users.noreply.github.com>
2025-03-15 15:42:00 -0300

    Add files via upload

--- Lógica de Programación/Desafios.py
+++ Lógica de Programación/Desafios.py
@@ -452,3 +452,49 @@ MiProgramador1.codigo()
 MiProgramador2.codigo()
 MiProgramador3.codigo()
 MiProgramador4.codigo()
+print("")
+
+'''
+Semana 10:
+DIFICULTAD EXTRA (opcional):
+Crea una función que sea capaz de procesar parámetros, pero que también
+pueda lanzar 3 tipos diferentes de excepciones (una de ellas tiene que
+corresponderse con un tipo de excepción creada por nosotros de manera
+personalizada, y debe ser lanzada de manera manual) en caso de error.
+ * - Captura todas las excepciones desde el lugar donde llamas a la función.
+ * - Imprime el tipo de error.
+ * - Imprime si no se ha producido ningún error.
+ * - Imprime que la ejecución ha finalizado.
+'''
+
+class StrTypeError(Exception):
+    pass
+
+def parametros_de_proceso(parametros: list):
+    if len(parametros) < 3:
+        raise IndexError()
+    elif parametros[1] == 0:
+        raise ZeroDivisionError()
+    elif type(parametros[2]) == "str":
+        raise StrTypeError("El tercer elemento no puede ser una cadena de texto.")
+
+    print(parametros[2])
+    print(parametros[0] / parametros[1])
+    print(parametros[2] + 5)
+
+try:
+    parametros_de_proceso([1, 2, 3, 4])
+except IndexError as e:
+    print("Error: El número de elementos de la lista debe ser mayor que dos.")
+except ZeroDivisionError as e:
+    print("Error: El segundo elemento de la lista no puede ser un cero.")
+except StrTypeError as e:
+    print(f"{e}")
+except Exception as e:
+    print(f"Se ha producido un error inesperado. {e}")
+else:
+    print("No se ha producido ningun error.")
+finally:
+    print("El programa ha finalizado sin detenerse.")
+
+print("")
