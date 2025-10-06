56ac6eaf7232d0316f14e9bd6285af0a7944ff6b
HotchkyssTomás <128932163+Hotchkyssdev@users.noreply.github.com>
2025-10-06 10:56:31 -0300

    Add files via upload

---
+++ Lógica de Programación/#00 SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/Java/Ejercicio1.java
@@ -0,0 +1,16 @@
+/*Ejercicio 1: Hola Mundo personalizado
+Crea un programa que muestre en pantalla el mensaje "Hola Mundo" y 
+luego solicite al usuario su nombre para imprimir "Hola, <nombre>!".*/
+import java.util.Scanner;
+
+public class Ejercicio1 {
+    public static void main(String[] args) {
+        String nombre;
+        Scanner input = new Scanner(System.in);
+        System.out.println("Hola Mundo");
+        System.out.println("\nIngrese su nombre: ");
+        nombre = input.nextLine();
+        System.out.println("\nHola " + nombre + "!");
+        input.close();
+    }
+}
---
+++ Lógica de Programación/#00 SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/Java/Hotchkyssdev.java
@@ -0,0 +1,27 @@
+//https://www.java.com/es/
+/*Este es un 
+comentario de 
+varias lineas
+*/
+
+public class Hotchkyssdev {
+    public static void main(String[] args) {
+        final double PI = 3.1415; //Constante
+        int entero = 5;
+        double doble = 5.99;
+        float flotante = 5.995f;
+        long largo = 150000L;
+        byte b =  127;
+        char caracter = 'G';
+        boolean booleano = true;
+        System.out.println("El valor de PI es: " + PI);
+        System.out.println("El valor de entero es: " + entero);
+        System.out.println("El valor de doble es: " + doble);
+        System.out.println("El valor de flotante es: " + flotante);
+        System.out.println("El valor de largo es: " + largo);
+        System.out.println("El valor de byte es: " + b);
+        System.out.println("El valor de caracter es: " + caracter);
+        System.out.println("El valor de booleano es: " + booleano);
+        System.out.println("Hola JAVA!");
+    }
+}
